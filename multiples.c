45ad33f0793bb1a3bbd12bf7580d9c7e73d8ee72
Maverich121 <mrtgrgl007@gmail.com>
2021-01-17 14:37:30 +0300

    third commit

---
+++ multiples.c
@@ -0,0 +1,19 @@
+// 4.11 Calcuating the sum of multiples.
+
+int main()
+{
+	int x,tmp,total = 0;
+
+	for(x=1;x<=100;x++)
+		if(x%7 == 0)
+		{
+			tmp = x;
+			total = tmp + total;
+		}
+
+	printf("%d\n",total);
+
+
+	system("pause");
+	return 0;
+}
